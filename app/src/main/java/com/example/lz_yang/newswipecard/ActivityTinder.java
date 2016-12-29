cabf8764da2f9aa2537635a551f0bbff1195cbd5
Lz-Yang <limzy-wa14@student.tarc.edu.my>
2016-12-29 16:17:40 +0800

    Only tinder swipe card from Placeholderview

---
+++ app/src/main/java/com/example/lz_yang/newswipecard/ActivityTinder.java
@@ -0,0 +1,95 @@
+package com.example.lz_yang.newswipecard;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.mindorks.butterknifelite.ButterKnifeLite;
+import com.mindorks.butterknifelite.annotations.BindView;
+import com.mindorks.butterknifelite.annotations.OnClick;
+import com.mindorks.placeholderview.SwipeDecor;
+import com.mindorks.placeholderview.SwipePlaceHolderView;
+
+import com.example.lz_yang.newswipecard.swipe.*;
+
+public class ActivityTinder extends AppCompatActivity {
+
+    @BindView(R.id.swipeView)
+    private SwipePlaceHolderView mSwipView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tinder_swipe);
+        ButterKnifeLite.bind(this);
+
+    }
+
+    @Override
+    protected void onPostCreate(@Nullable Bundle savedInstanceState) {
+        super.onPostCreate(savedInstanceState);
+        mSwipView.disableTouchSwipe();
+        mSwipView.getBuilder()
+//                .setSwipeType(SwipePlaceHolderView.SWIPE_TYPE_VERTICAL)
+                .setDisplayViewCount(3)
+                .setIsUndoEnabled(true)
+                .setWidthSwipeDistFactor(15)
+                .setHeightSwipeDistFactor(20)
+                .setSwipeDecor(new SwipeDecor()
+//                        .setMarginTop(300)
+//                        .setMarginLeft(100)
+//                        .setViewGravity(Gravity.TOP)
+                        .setPaddingTop(20)
+                        .setRelativeScale(0.01f)
+                        .setSwipeInMsgLayoutId(R.layout.tinder_swipe_in_msg_view)
+                        .setSwipeOutMsgLayoutId(R.layout.tinder_swipe_out_msg_view));
+        TinderCard tinderCard = new TinderCard();
+
+        mSwipView.addView(tinderCard)
+                .addView(new TinderCard())
+                .addView(new TinderCard())
+                .addView(new TinderCard())
+                .addView(new TinderCard())
+                .addView(new TinderCard())
+                .addView(new TinderCard())
+                .addView(new TinderCard())
+                .addView(new TinderCard())
+                .addView(new TinderCard());
+
+        tinderCard.setProfileImageView(R.drawable.yoona1);
+
+        new Thread(new Runnable(){
+            @Override
+            public void run() {
+                try {
+                    Thread.currentThread().sleep(8000);
+                    mSwipView.enableTouchSwipe();
+//                    mSwipView.lockViews();
+//                    Thread.currentThread().sleep(4000);
+//                    mSwipView.unlockViews();
+//                    Thread.currentThread().sleep(4000);
+//                    mSwipView.lockViews();
+//                    Thread.currentThread().sleep(4000);
+//                    mSwipView.unlockViews();
+                }catch (InterruptedException e){
+                    e.printStackTrace();
+                }
+            }
+        }).start();
+    }
+
+    @OnClick(R.id.rejectBtn)
+    private void onRejectClick(){
+        mSwipView.doSwipe(false);
+    }
+
+    @OnClick(R.id.acceptBtn)
+    private void onAcceptClick(){
+        mSwipView.doSwipe(true);
+    }
+
+    @OnClick(R.id.undoBtn)
+    private void onUndoClick(){
+        mSwipView.undoLastSwipe();
+    }
+}
